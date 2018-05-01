428f912bd7c017630a4029876c79ff6fa7f49a5b
Ramon Borges <ramontricolor08@hotmail.com>
2018-04-30 21:51:56 -0300

    Setup do projeto

---
+++ jsf-livraria/src/java/br/com/caelum/livro/bean/LivroBean.java
@@ -0,0 +1,54 @@
+package br.com.caelum.livro.bean;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author ramonbs
+ */
+
+@ManagedBean
+public class LivroBean {
+    
+    private String titulo;
+    private String isbn;
+    private double preco;
+    private String dataLancamento;
+    
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+    
+    public void gravar() {
+        System.out.println("Gravando Livro" + this.titulo);
+    }
+}
